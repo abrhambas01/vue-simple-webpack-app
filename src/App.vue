a593bb6b2eca0e7a88e9c38912c315a523959180
Abrham Bas <abrham.bas01@gmail.com>
2017-04-15 12:30:56 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="app">
+		<app-message>Welcome to your app</app-message>
+	</div>
+</template>
+<script>
+	import Message from './components/Message.vue';
+	export default {
+		components : { 
+			'app-message' : Message 
+		}
+	}
+</script>
+<style>
+	#app {
+		margin : 27px;
+		background-color : #eaeaea;
+		color : black;
+	}
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h1>My First Vue App</h1>
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+
+</script>
+<style>
+	h1 { 
+		font-family : 'Arial';
+		color : black;
+		font-size : 50px;
+	}
+
+	
+</style>
+
